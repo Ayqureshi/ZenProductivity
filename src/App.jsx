1bbff41d9142dcca1285833447a5b8981aad1291
alamgir09 <alamgir@usc.edu>
2023-07-29 17:04:22 +0300

    added community page and styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,11 @@ import Articles from './components/Articles';
 import Interviews from './components/Interviews';
 import Resources from './components/Resources';
 import Loggingin from './components/loggingin';
-import CommunityForum from './components/CommunityForum';
+import Community from './components/Community';
 import Questions from './components/auth/questionPage';
+import SignUp from './components/auth/SignUp';
+import ResetPassword from './components/auth/ResetPassword';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
@@ -26,7 +29,7 @@ const App = () => {
             <Link to="/interviews">Audio Interviews</Link>
           </li>
           <li>
-            <Link to="/community-forum">Community Forum</Link>
+            <Link to="/community">Community</Link>
           </li>
           <li>
             <Link to="/resources">Resources</Link>
@@ -42,8 +45,8 @@ const App = () => {
         <Route path="/articles" element={<Articles />} />
         <Route path="/interviews" element={<Interviews />} />
         <Route path="/resources" element={<Resources />} />
-        <Route path="/community-forum" element={<CommunityForum />} />
-        <Route path="/loggingin/*" element={<Loggingin />} />
+        <Route path="/community" element={<Community />} />
+        <Route path="/loggingin" element={<Loggingin />} />
         <Route path="/questions" element={<Questions />} />
       </Routes>
     </Router>
