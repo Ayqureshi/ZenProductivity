fffe067a9ccf4e549a740f09f25d497016ec813d
Yingxin Wu <wuyingxi@usc.edu>
2023-10-28 14:37:21 -0700

    update CareerSuccess.js with audio buttons and slider

--- src/components/CareerSuccess.js
+++ src/components/CareerSuccess.js
@@ -5,6 +5,9 @@ import img5 from '../images/microphone 1.png';
 import img6 from '../images/mic-transparent-background.png';
 import img7 from '../images/playbutton.png';
 import PropTypes from 'prop-types'; // Import PropTypes for validation
+import {storage} from '../firebase'; // storage initialized in firebase.js
+import {ref, getDownloadURL} from "firebase/storage";
+import {useEffect, useState, useRef} from "react";
 
 function CareerSuccess({
   backgroundColor = "#E47C42",
@@ -19,6 +22,59 @@ function CareerSuccess({
     { title: "Career Success Title 6", description: "Description for episode 6" }
   ]
 }) {
+  const [audioURL, setAudioURL] = useState(null);
+  const [audioStatus, changeAudioStatus] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const audioRef = useRef();
+
+  useEffect(() => {
+    const mp3Ref = ref(storage, 'audio/testing.mp3'); // get reference to where audio is stored
+
+    getDownloadURL(mp3Ref)
+      .then((url) => {
+        setAudioURL(url); // if successful, update the state of audioURL
+      })
+      .catch((error) => {
+        console.error("Error downloading the MP3 file:", error);
+      });
+  }, []);
+
+  const startAudio = () => {
+    audioRef.current.play();
+    changeAudioStatus(true);
+  };
+
+  const pauseAudio = () => {
+    audioRef.current.pause();
+    changeAudioStatus(false);
+  };
+
+  const handleForward30Seconds = () => {
+    if (audioRef.current) {
+      console.log("forwarding??");
+      console.log(audioRef.current.currentTime);
+      audioRef.current.currentTime += 30;
+    }
+  };
+
+  const handleBackward30Seconds = () => {
+    if (audioRef.current) {
+      console.log("going back??");
+      audioRef.current.currentTime -= 30;
+    }
+  };
+
+  const handleTimeUpdate = () => {
+    setCurrentTime(audioRef.current.currentTime);
+  };
+
+  const handleSliderChange = (e) => {
+    const newTime = parseFloat(e.target.value);
+    audioRef.current.currentTime = newTime;
+    setCurrentTime(newTime);
+  };
+
+
   return (
     <>
       <div className="rectangle" style={{ backgroundColor: backgroundColor }}>
@@ -46,6 +102,30 @@ function CareerSuccess({
               <div className="audio-content">
                 <h3><Link to={`/episodes/${index}`}>{episode.title}</Link></h3>
                 <p>{episode.description}</p>
+                <div>
+                  {audioURL ? (
+                    <audio ref={audioRef} src={audioURL} onTimeUpdate={handleTimeUpdate}>
+                    </audio>
+                  ) : (
+                    'Loading...' // when getting source
+                  )}
+                  <input
+                    type="range"
+                    min="0"
+                    max={audioRef.current ? audioRef.current.duration : 0}
+                    step="1"
+                    value={currentTime}
+                    onChange={handleSliderChange}
+                  />
+                </div>
+                  <button onClick={handleForward30Seconds}>Forward 30 seconds</button>
+                  {/* adding buttons */}
+                  {audioStatus ? (
+                    <button onClick={pauseAudio}>pause</button>
+                  ) : (
+                    <button onClick={startAudio}>start</button>
+                  )}
+                <button onClick={handleBackward30Seconds}>Backward 30 seconds</button>
               </div>
             </li>
           ))}
