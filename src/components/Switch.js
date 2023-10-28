9a6d8eb756f7cb1d1f7f36392ec1d51c77c49750
kathylinn <kathylin924@gmail.com>
2023-10-28 14:29:57 -0700

    Extended home page

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -3,7 +3,7 @@ import './Switch.css';
 const Switch = () => {
     return(
         <><div className="switch"><a href="index.html">LOG IN</a></div>
-        <div className="button"><a href="index.html">SIGN UP</a></div></>
+        <div className="button"><a href="/components/Articles">SIGN UP</a></div></>
     );
 };
 
