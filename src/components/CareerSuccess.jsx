3d1a343277896fa3dee3792d0a999852a121f775
Yingxin Wu <wuyingxi@usc.edu>
2023-11-12 14:22:03 -0800

    fixed audio

--- src/components/CareerSuccess.jsx
+++ src/components/CareerSuccess.jsx
@@ -5,128 +5,31 @@ import './CareerSuccess.css';
 import img8 from '../images/microphone.png';
 import careersuccess from '../images/careerstories.png';
 import PropTypes from 'prop-types'; // Import PropTypes for validation
-import {storage} from '../firebase'; // storage initialized in firebase.js
-import {ref, getDownloadURL} from "firebase/storage";
-import {useState, useRef, useEffect} from "react";
 
 
 function CareerSuccess({
+  customTextSectionClass = "text-section",
+  customTextSectionContent = <h1>Career Success Stories</h1>,
   episodes = [
-    { title: "Career Success Title 1", description: "Description for episode 1" },
-    { title: "Career Success Title 2", description: "Description for episode 2" },
-    { title: "Career Success Title 3", description: "Description for episode 3" },
+    { title: "Ep. 1 - Zuck on Harvard", description: "Mark Zuckerberg talks about how he started Facebook. And yes, he adamantly states that the Winklevoss twins are a fraud." },
+    { title: "Ep. 2 - Jeff Bezos Plotting World Domination", description: "Streaming? Cloud computing? Online retailer? Groceries? What else does Bezos have in mind? The grind never stops..." },
+    { title: "Ep. 3 - Bill Gates Dealing With Divorce In the Public Eye", description: "The tea everyone’s been waiting for. Why did Bill cheat on Melinda?" },
     { title: "Career Success Title 4", description: "Description for episode 4" },
     { title: "Career Success Title 5", description: "Description for episode 5" },
     { title: "Career Success Title 6", description: "Description for episode 6" }
   ]
 }) {
   const navigate = useNavigate();
-
-  const [loadedEpisodes, setLoadedEpisodes] = useState([]);
-  const [audioData, setAudioData] = useState(episodes.map(() => ({
-    audioURL: null,
-    audioStatus: false,
-    currentTime: 0,
-    audioRef: useRef(null),
-  })));
-
-  // Load all audio files when the component is mounted
-  useEffect(() => {
-    episodes.forEach((episode, index) => {
-      loadEpisodeAudio(index);
-    });
-  }, []);
-  
-  const loadEpisodeAudio = (index) => {
-    const mp3Ref = ref(storage, `audio/career_episode${index + 1}.mp3`);
-
-    getDownloadURL(mp3Ref)
-      .then((url) => {
-        setAudioData((prevAudioData) =>
-          prevAudioData.map((item, i) =>
-            i === index ? { ...item, audioURL: url } : item
-          )
-        );
-        setLoadedEpisodes((prevLoadedEpisodes) => [...prevLoadedEpisodes, index]);
-      })
-      .catch((error) => {
-        console.error(`Error downloading the MP3 file for episode ${index + 1}:`, error);
-      });
-  };
-
-  const isEpisodeLoaded = (index) => loadedEpisodes.includes(index);
-
-  const startAudio = (index) => {
-    if (audioData[index].audioRef.current) {
-      audioData[index].audioRef.current.play();
-      setAudioData((prevAudioData) =>
-        prevAudioData.map((item, i) =>
-          i === index ? { ...item, audioStatus: true } : item
-        )
-      );
-    }
-  };
-  
-  const pauseAudio = (index) => {
-    audioData[index].audioRef.current.pause();
-    setAudioData((prevAudioData) =>
-      prevAudioData.map((item, i) =>
-        i === index ? { ...item, audioStatus: false } : item
-      )
-    );
-  };
-  
-  const handleForward30Seconds = (index) => {
-    if (audioData[index].audioRef.current) {
-      audioData[index].audioRef.current.currentTime += 30;
-    }
-  };
-  
-  const handleBackward30Seconds = (index) => {
-    if (audioData[index].audioRef.current) {
-      audioData[index].audioRef.current.currentTime -= 30;
-    }
-  };
-  
-  const handleSliderChange = (e, index) => {
-    const newTime = parseFloat(e.target.value);
-    audioData[index].audioRef.current.currentTime = newTime;
-    setAudioData((prevAudioData) =>
-      prevAudioData.map((item, i) =>
-        i === index ? { ...item, currentTime: newTime } : item
-      )
-    );
-  };
-
-  const handleTimeUpdate = (index) => {
-    console.log(`handleTimeUpdate: Index ${index}`);
-    if (audioData[index] && audioData[index].audioRef) {
-      const audioRef = audioData[index].audioRef.current;
-      if (audioRef) {
-        const currentTime = audioRef.currentTime;
-        setAudioData((prevAudioData) =>
-          prevAudioData.map((item, i) =>
-            i === index ? { ...item, currentTime } : item
-          )
-        );
-      } else {
-        console.log(`handleTimeUpdate: Invalid audioRef for index ${index}`);
-      }
-    } else {
-      console.log(`handleTimeUpdate: Invalid index or audioData for index ${index}`);
-    }
-  };  
-  
   return (
     <>
-      <div className="text-section">
-        <h1>Career Success Stories</h1>
-      </div>
-      <img src={careersuccess} className="career-image" alt="Career Success" />
-
+        <div className={customTextSectionClass}>
+          {customTextSectionContent}
+        </div>
+        <img src={careersuccess} className="career-image"></img>
+      
       <div className="episodes">
         <button className="back" onClick={() => navigate(-1)}>
-          ← All Podcasts
+            ← All Podcasts
         </button>
         <ul className="custom-list">
           {episodes.map((episode, index) => (
@@ -134,54 +37,22 @@ function CareerSuccess({
               <span className="number">{index + 1}.</span>
               <img src={img8} alt={`Description of Image ${index + 1}`} className="item-image" />
               <div className="audio-content">
-                <h3>
-                  <Link to={`/episodes/${index}`}>{episode.title}</Link>
-                </h3>
+                <h3><Link to={`/episodes/${index}`}>{episode.title}</Link></h3>
                 <p>{episode.description}</p>
-                <div>
-                  {isEpisodeLoaded(index) ? (
-                    <audio
-                      ref={audioData[index].audioRef}
-                      src={audioData[index].audioURL}
-                      onTimeUpdate={() => handleTimeUpdate(index)}
-                    ></audio>
-                  ) : (
-                    <button onClick={() => loadEpisodeAudio(index)}>Load Audio</button>
-                  )}
-                  <input
-                    type="range"
-                    min="0"
-                    max={
-                      audioData[index].audioRef.current
-                        ? audioData[index].audioRef.current.duration
-                        : 0
-                    }
-                    step="1"
-                    value={audioData[index].currentTime}
-                    onChange={(e) => handleSliderChange(e, index)}
-                  />
-                </div>
-                <button onClick={() => handleForward30Seconds(index)}>
-                  Forward 30 seconds
-                </button>
-                {audioData[index].audioStatus ? (
-                  <button onClick={() => pauseAudio(index)}>Pause</button>
-                ) : (
-                  <button onClick={() => startAudio(index)}>Start</button>
-                )}
-                <button onClick={() => handleBackward30Seconds(index)}>
-                  Backward 30 seconds
-                </button>
               </div>
             </li>
           ))}
         </ul>
       </div>
     </>
+    
   );
 }
 
+// Add PropTypes validation
 CareerSuccess.propTypes = {
+  customTextSectionClass: PropTypes.string,
+  customTextSectionContent: PropTypes.string,
   backgroundColor: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string,
--- src/components/Episodes.jsx
+++ src/components/Episodes.jsx
@@ -1,20 +1,51 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import './Episodes.css';
 import image from '../images/marie-curie.png';
 import forwardbutton from '../images/forwardbutton.png';
 import backwardbutton from '../images/backwardbutton.png';
 import playbutton from '../images/playbutton.webp';
 import pausebutton from '../images/pausebutton.png';
-import { useState, useRef} from "react";
+import {storage} from '../firebase'; // storage initialized in firebase.js
+import {ref, getDownloadURL} from "firebase/storage";
+import { useEffect, useState, useRef} from "react";
 
 
 function Episodes() {
     const navigate = useNavigate();
-    const audioURL = null;
+    const { episodeNumber } = useParams();
+    const episodeContent = [
+      { title: "Episode 1", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+      url: "audio/career_episode1.mp3"},
+      { title: "Episode 2", description: "Description for episode 2",
+      url: "audio/career_episode2.mp3" },
+      { title: "Episode 3", description: "Description for episode 3",
+      url: "audio/career_episode3.mp3" },
+      { title: "Episode 4", description: "Description for episode 4",
+      url: "audio/career_episode4.mp3" },
+      { title: "Episode 5", description: "Description for episode 5",
+      url: "audio/career_episode5.mp3" },
+      { title: "Episode 6", description: "Description for episode 6",
+      url: "audio/career_episode6.mp3" }
+    ];
+    const episodeNumberInt = parseInt(episodeNumber);
+    console.log("Episode Number: " + episodeNumber);
+    const [audioURL, setAudioURL] = useState(null);
     const [audioStatus, changeAudioStatus] = useState(false);
     const [currentTime, setCurrentTime] = useState(0);
     const audioRef = useRef();
+    useEffect(() => {
+      const mp3Ref = ref(storage, episodeContent[episodeNumberInt].url); // get reference to where audio is stored
+  
+      getDownloadURL(mp3Ref)
+        .then((url) => {
+          setAudioURL(url); // if successful, update the state of audioURL
+        })
+        .catch((error) => {
+          console.error("Error downloading the MP3 file:", error);
+        });
+    }, []);
+
     const startAudio = () => {
         audioRef.current.play();
         changeAudioStatus(true);
@@ -27,7 +58,6 @@ function Episodes() {
     
       const handleForward30Seconds = () => {
         if (audioRef.current) {
-          console.log("forwarding??");
           console.log(audioRef.current.currentTime);
           audioRef.current.currentTime += 30;
         }
@@ -54,13 +84,13 @@ function Episodes() {
         <>
         <div id="title">
             <div className="episode1-title">
-                <h1>Episode 6 - Marie Curie’s Journey</h1>
+                <h1>{episodeContent[episodeNumberInt].title}</h1>
             </div>
         </div>
         <div id="individual-episode">
                 <img src={image} alt="Episode Description" className="microphone-image" />
                 <div className="text-box">
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+                    <p>{episodeContent[episodeNumberInt].description}</p>
                 </div>
                 <div className="episode1-player">
                 <div>
@@ -104,9 +134,7 @@ function Episodes() {
                     onClick={handleForward30Seconds}
                     className="forward-button" 
                 />
-              
-
-
+            
                 </div>
         </div>
         <button className="back-button" onClick={() => navigate(-1)}>
